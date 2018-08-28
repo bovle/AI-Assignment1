4930b641976aceda445690a2b044fb1dcdcc432a
Frederik Lund <frederik.bl@live.dk>
2018-08-28 15:24:51 +1000

    bug fix

--- src/problem/Main.java
+++ src/problem/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
         ProblemSpec ps = new ProblemSpec();
         try {
-            ps.loadProblem("input1.txt");
-            ps.loadSolution("output1.txt");
+            ps.loadProblem(args[0]);
+            ps.loadSolution(args[1]);
         } catch (IOException e) {
             System.out.println("IO Exception occured");
         }
